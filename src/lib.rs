8f80a8a716040f4c827280019ab9056b10468bac
abharun <adambinharun1010@gmail.com>
2024-03-15 04:32:28 +0100

    implement simple async threading

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod mythreading;
+
+pub use mythreading::threading;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use my_rust_course::threading;
+
 fn main() {
     println!("Hello, Rustaceans!\nWelcome to my Rust course to improve my Rust coding skills!\n");
+
+    threading::thread_factory(10);
 }
