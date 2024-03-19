88f045873fefd21f95915513afa25773dae58f08
abharun <adambinharun1010@gmail.com>
2024-03-19 04:04:22 +0100

    echo from user input

--- src/functions/echo_channel.rs
+++ src/functions/echo_channel.rs
@@ -1,4 +1,30 @@
+use std::io;
+use std::sync::mpsc;
+use std::thread;
 
 pub fn echo_with_channel() {
-    println!("Echo with Channel");
+    let mut input = String::new();
+
+    let stdin = io::stdin();
+
+    let (tx, lx) = mpsc::channel::<String>();
+
+    let _ = thread::spawn(move || {
+        while let Ok(recv_str) = lx.recv() {
+            if recv_str == String::from("bye") {
+                break;
+            }
+            println!("Your Input: {:?}", recv_str);
+        }
+    });
+
+    while let Ok(_) = stdin.read_line(&mut input) {
+        input = input.trim().to_string();
+        tx.send(input.clone()).unwrap();
+        if input == String::from("bye") {
+            break;
+        }
+        input.clear();
+    }
+    println!("Echo function ended");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ use my_rust_course::interface;
 use std::io;
 
 fn main() {
-    println!("Hello, Rustaceans!\nWelcome to my Rust course to improve my Rust coding skills!\n");
+    println!("Hello, Rustaceans!\nWelcome to my Rust course to improve my Rust coding skills!\nYou can see the index for particular functionality from README.md\nInput func index:");
 
     let mut stdin = io::stdin();
     let mut input = String::new();
 
     while let Ok(_) = stdin.read_line(&mut input) {
         let index = input.trim().parse();
-        println!("{:?}", index);
         match index.unwrap() {
             1 => { interface::threading_interface(); },
             2 => { interface::echo_channel_interface(); }
@@ -18,5 +17,6 @@ fn main() {
             }
         };
         input.clear();
+        println!("Input func index:");
     }
 }
